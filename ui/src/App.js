b084062752fd49ed0628b46ba4022b59e91c55dc
jihoonshin <jip68660@gmail.com>
2019-11-23 12:14:14 -0600

    Display web-cam

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataUri: null
+    }; 
+    this.onTakePhotoAnimationDone = this.onTakePhotoAnimationDone.bind(this);
+    this.retakePhoto = this.retakePhoto.bind(this);
+  }
+  onTakePhotoAnimationDone (dataUri) {
+    console.log('Take Photo');
+    this.setState({
+      dataUri: dataUri
+    });
+  }
+  retakePhoto = () => {
+    this.setState({
+      dataUri: null
+    });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Mood Music</h1>
+        {
+          (this.state.dataUri) 
+          ? <div>
+              <img src={ this.state.dataUri } />
+              <button
+                className={ "retake"}
+                onClick={ this.retakePhoto }
+              >
+              Want to take another one?
+              </button>
+            </div>
+          : <Camera
+            onTakePhotoAnimationDone = { this.onTakePhotoAnimationDone } 
+          />
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
