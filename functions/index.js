3ee1cf0bf7585f8243907206b1ac4a3ba290ead2
jihoonshin <jip68660@gmail.com>
2019-11-30 13:14:17 -0600

    Change index

--- functions/index.js
+++ functions/index.js
@@ -11,14 +11,14 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.face = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  const snapshot = await admin.database().ref('/messages').push({original: original});
-  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-  res.redirect(303, snapshot.ref.toString());
-});
+// exports.face = functions.https.onRequest(async (req, res) => {
+//   // Grab the text parameter.
+//   const original = req.query.text;
+//   // Push the new message into the Realtime Database using the Firebase Admin SDK.
+//   const snapshot = await admin.database().ref('/messages').push({original: original});
+//   // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+//   res.redirect(303, snapshot.ref.toString());
+// });
 
 app.post('/', function(req, res) {
   console.log('handling /');
@@ -45,3 +45,5 @@ app.post('/', function(req, res) {
     console.log(`    Surprise: ${face.surpriseLikelihood}`);
   });
 });
+
+exports.face = functions.https.onRequest(app);
