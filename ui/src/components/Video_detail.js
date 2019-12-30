85fc8f414f519dbd96b288a284fb224ba4af5003
Jihoon Shin <jihoonshin@Jihoons-MacBook-Pro.local>
2019-12-30 14:38:40 -0600

    Decode special entities

--- ui/src/components/Video_detail.js
+++ ui/src/components/Video_detail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCompactDisc } from '@fortawesome/free-solid-svg-icons';
+import decode from 'unescape';
 
 const Video_detail = ({ video }) => {
     if (!video) {
@@ -9,12 +10,15 @@ const Video_detail = ({ video }) => {
 
     const videoId = video.id.videoId;
     const url = `https://www.youtube.com/embed/${videoId}`;
+    console.log(video.snippet.title);
+    const text = video.snippet.title;
+    console.log(decode(text));
 
     return (
         <div className="displayed">
             <iframe src= { url }></iframe>
             <div className="displayed_title">
-                {video.snippet.title}
+                { decode(video.snippet.title) }
             </div>
         </div>
     )
